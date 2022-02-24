c3558bc7ed16c34f1e020b40e189617d223d22a8
Xogo8565 <Xogo8565@gmail.com>
2022-02-25 03:57:35 +0900

    1

---
+++ Xogo8565.github.io/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Day01_Java",
+        "excerpt":"자바 출력문   자바 변수   ","categories": ["java","study"],
+        "tags": ["java","study"],
+        "url": "/java/study/Day01_java/",
+        "teaser": null
+      },{
+        "title": "Github.io 블로그 시작하기",
+        "excerpt":"GitHub Blog 서비스인 github.io 블로그를 개설하였다. 공부하면서 하나씩 차근차근 정리해서 올리는 것이 목표다. 블로그 글 작성하는 법 _post 폴더에 md 파일을 생성하고 글을 작성한다. 지킬 서버를 열어 모니터링을 해본다. cd [블로그 레파지토리] bundle exec jekyll serve –incremental 마음에 든다면 깃에 푸쉬까지. cd [블로그 레파지토리] git add . git commit -m...","categories": ["blog"],
+        "tags": ["content","blog"],
+        "url": "/blog/github.io-%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0/",
+        "teaser": null
+      }]
