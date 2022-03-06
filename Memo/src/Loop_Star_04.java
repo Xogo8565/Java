d91852661fdc451bf91e9559c0659ccde22bcc27
Xogo8565 <Xogo8565@gmail.com>
2022-03-06 18:28:24 +0900

    Loop_Practice

---
+++ Memo/src/Loop_Star_04.java
@@ -0,0 +1,17 @@
+
+public class Loop_Star_04 {
+    /*  *****
+        ****
+        ***
+        **
+        *
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= 6 - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
